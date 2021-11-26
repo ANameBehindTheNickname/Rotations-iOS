2bd9883511c166b85f84500d20c4a13cd007a845
ANameBehindTheNickname <name.behind.nickname@gmail.com>
2021-11-26 15:35:23 +0200

    Update UILabel based on the device orientation

--- SampleApp/ViewController.swift
+++ SampleApp/ViewController.swift
@@ -4,14 +4,33 @@
 //
 
 import UIKit
+import Rotations
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var orientationLabel: UILabel!
+    let rotationsManager = RotationManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        rotationsManager.startRotationManagerUpdates(self)
     }
-
-
 }
 
+extension ViewController: RotationManagerDelegate {
+    func deviceDidRotate(to orientation: UIDeviceOrientation) {
+        var orientationText = ""
+        switch orientation {
+        case .unknown: orientationText = "unknown"
+        case .landscapeLeft: orientationText = "landscapeLeft"
+        case .landscapeRight: orientationText = "landscapeRight"
+        case .portrait: orientationText = "portrait"
+        case .portraitUpsideDown: orientationText = "portraitUpsideDown"
+        default: break
+        }
+        
+        DispatchQueue.main.async {
+            self.orientationLabel.text = orientationText
+        }
+    }
+}
